1b0b2740b02df0296079c309820a39d69c99d848
Carlos Santana <carlillos136@gmail.com>
2016-10-16 21:18:06 -0500

    Add adapter and model

---
+++ app/src/main/java/com/csantana/tipcalc/models/TipRecord.java
@@ -0,0 +1,48 @@
+package com.csantana.tipcalc.models;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by CLsantana on 16/10/16.
+ */
+
+public class TipRecord {
+
+    private double bill;
+        private int tipPercentage;
+        private Date timestamp;
+
+                public double getBill() {
+                return bill;
+            }
+
+                public void setBill(double bill) {
+                this.bill = bill;
+            }
+
+                public int getTipPercentage() {
+                return tipPercentage;
+            }
+
+                public void setTipPercentage(int tipPercentage) {
+                this.tipPercentage = tipPercentage;
+            }
+
+                public Date getTimestamp() {
+                return timestamp;
+            }
+
+                public void setTimestamp(Date timestamp) {
+                this.timestamp = timestamp;
+            }
+
+                public double getTip() {
+                return bill * (tipPercentage/100d);
+            }
+
+                public String getDateFormated() {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM dd, yyyy HH:mm");
+                return simpleDateFormat.format(timestamp);
+            }
+}
